50adb70c9b84836c2f46a4b346407db0685f5c70
Yonatan Haddish <yonihailshn@gmail.com>
2024-10-09 12:44:53 -0400

    test scan security001

--- sample.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-	char last_name[20];
-	printf ("Enter your last name: ");
-	scanf ("%s", last_name);
-	printf("Entered last name is %s\n", last_name);
-	}
---
+++ testCode.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Simulated user database
+const users = [];
+
+// Vulnerable registration endpoint
+app.post("/register", (req, res) => {
+  const { username } = req.body;
+
+  // No proper validation on username
+  users.push(username); // Vulnerable to injection attacks
+  res.send(`User ${username} registered successfully!`);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
